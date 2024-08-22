cc32aacc1b72975f3baaeec5e906933191fa5931
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-08-22 15:55:14 -0500

    Corrigiendo ejercicio de determinar el número mayor

--- Ejercicio_2.rb
+++ Ejercicio_2.rb
@@ -12,12 +12,12 @@ numero2 = gets.to_i
 puts "Escriba el valor de numero3: " 
 numero3 = gets.to_i
 
-if numero1 > numero2 && numero3
+if numero1 > numero2 && numero1 > numero3
     puts "El numero mayor es #{numero1}"
     
-elsif numero2 > numero1 && numero3
+elsif numero2 > numero1 && numero2 > numero3
     puts "El numero mayor es #{numero2}"
 
-else
+elsif numero3 > numero1 && numero3 > numero2 
     puts "El numero mayor es #{numero3}"
 end
