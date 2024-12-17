4866bf3dd649fbae6f1a071dd24039398c99b54e
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-12-17 13:14:51 -0500

    Aprendiendo

---
+++ Ejercicio_9.rb
@@ -0,0 +1,20 @@
+=begin
+
+Utilizando Programación Orientada a Objetos (POO),
+crea una clase llamada Carro que cuente con los atributos que consideres necesarios.
+Además, implementa métodos como arrancar, detenerse, y otros que reflejen el comportamiento de un carro.
+
+=end
+
+class Carro 
+    
+    def initialize(marca, modelo, color, n_puertas)
+        @marca = marca
+        @modelo = modelo
+        @color = color
+        @n_puertas = n_puertas
+    end
+
+    
+
+end
