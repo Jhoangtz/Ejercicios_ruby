f15ed4d6767c3a6d36bb3e5642af9f4bc8b7008a
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-12-17 13:28:59 -0500

    Aprendiendo

--- Ejercicio_9.rb
+++ Ejercicio_9.rb
@@ -15,6 +15,20 @@ class Carro
         @n_puertas = n_puertas
     end
 
-    
+    def marca
+        puts "La marca de el carro es: #{marca}"
+    end
+
+    def modelo
+        puts "El modelo de el carro es: #{modelo} "
+    end
+
+    def color
+        puts "El color de el carro es: #{color}"
+    end
+
+    def n_puertas
+        puts "El numero de puertas de el carro es: #{n_puertas}"
+    end
 
 end
