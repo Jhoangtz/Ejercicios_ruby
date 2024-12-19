6766854a3c71c799a05346556ded86241029e27d
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-12-19 18:29:26 -0500

    Aprendiendo

---
+++ Ejercicio_10.rb
@@ -0,0 +1,54 @@
+=begin
+
+Usando programacion orientada a objetos, crea una clase Calculadora,
+las cuales debe tener dos atributos, num1 y num2, luego de esto,
+implementa metodos tales como, sumar, restar, multiplicar, dividir, raiz cuadrada
+
+=end
+
+class Calculadora
+    
+    attr_reader :num1, :num2
+
+    def initialize(num1, num2)
+        @num1 = num1
+        @num2 = num2
+    end
+
+    def sumar
+        suma = num1 + num2
+        puts "La suma de num1 y num2 es: #{suma}"
+    end
+
+    def restar
+        resta = num1 - num2
+        puts "La resta de num1 y num es: #{resta}"
+    end
+
+    def multiplicar
+        multiplicación = num1 * num2
+        puts "La multiplicación de num1 y num es: #{multiplicación}"
+    end
+
+    def dividir
+        divición = num1 / num2
+        puts "La divición de num1 y num2 es: #{divición}"
+    end
+
+    def raiz_cuadrada
+        raiz_cuadrada_num1 = Math.sqrt(num1)
+        puts "La raiz cuadrada de num1 es #{raiz_cuadrada_num1}"
+
+        raiz_cuadrada_num2 = Math.sqrt(num2)
+        puts "La raiz cuadrada de num2 es: #{raiz_cuadrada_num2}"
+    end
+
+end
+
+calculadora1 = Calculadora.new(10, 5)
+
+calculadora1.sumar
+calculadora1.restar
+calculadora1.multiplicar
+calculadora1.dividir
+calculadora1.raiz_cuadrada
