ddc9b886f57536ad8da75da400a415a8709149bd
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-12-19 16:06:20 -0500

    Aprendiendo

--- Ejercicio_9.rb
+++ Ejercicio_9.rb
@@ -8,27 +8,50 @@ Además, implementa métodos como arrancar, detenerse, y otros que reflejen el c
 
 class Carro 
     
-    def initialize(marca, modelo, color, n_puertas)
+    attr_reader :marca, :modelo, :color, :num_puertas
+
+    def initialize(marca, modelo, color, num_puertas)
         @marca = marca
         @modelo = modelo
         @color = color
-        @n_puertas = n_puertas
+        @num_puertas = num_puertas
     end
 
-    def marca
+    def ver_marca
         puts "La marca de el carro es: #{marca}"
     end
 
-    def modelo
+    def ver_modelo
         puts "El modelo de el carro es: #{modelo} "
     end
 
-    def color
+    def ver_color
         puts "El color de el carro es: #{color}"
     end
 
-    def n_puertas
-        puts "El numero de puertas de el carro es: #{n_puertas}"
+    def ver_num_puertas
+        puts "El numero de puertas de el carro es: #{num_puertas}"
+    end
+
+    def arrancar
+        puts "El carro está arrancando"
+    end
+
+    def detenerse
+        puts "El carro se detuvo"
+    end
+
+    def abrir_puertas
+        puts "Las puertas se abrieron"
+    end
+
+    def cerrar_puertas
+        puts "Las puertas se cerraron"
     end
 
 end
+
+carro1 = Carro.new("nissan", "2014", "gris", 4)
+
+carro1.arrancar
+carro1.ver_marca
